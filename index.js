772e29f54b512de54792b074dc16ab7bb84acbbb
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-04 15:53:12 -0500

    stuffs

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
-function autoAdjustBackstretch(){
+function autoAdjustBackstretch() {
   if ($("#backstretch-img")[0].width / $("#backstretch-img")[0].height < $(window).width() / $(window).height()) {
     $("#backstretch-img").css({
-        "width": "100%",
-        "height": "auto"
+      "width": "100%",
+      "height": "auto"
     });
   } else {
     $("#backstretch-img").css({
-        "width": "auto",
-        "height": "100%"
+      "width": "auto",
+      "height": "100%"
     });
   }
 }
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   $("body").removeClass("preload");
   $("#nav-placeholder").load("./nav.html");
   $("#footer-placeholder").load("./footer.html");
   // autoAdjustBackstretch();
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >100) {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
       $("nav").addClass("bg-light");
       $("nav").addClass("navbar-light");
       $("nav").removeClass("navbar-dark");
@@ -32,8 +32,15 @@ $(document).ready(function() {
     }
   });
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     // autoAdjustBackstretch();
   });
 
 });
+
+function scrollToID(targetID) {
+  console.log("scrolling to " + targetID);
+  $('html,body').animate({
+    scrollTop: $("#" + targetID).offset().top - 100
+  });
+}
