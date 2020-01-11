536624e7f0df90433951f542cce7b1641765966e
Ynng <44907675+Ynng@users.noreply.github.com>
2020-01-11 01:05:20 -0500

    made members hover to open

--- index.js
+++ index.js
@@ -75,6 +75,20 @@ function checkBrowser() {
   $("body").addClass(browser);
 }
 
+function toggleDropdown (e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function(){
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 300 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);
 
 $(document).ready(function () {
   checkBrowser();
